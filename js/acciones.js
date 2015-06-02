99145654dd2daf4c087306d4623668fdb823cf3f
AL40F13 <soriano11.ans@gmail.com>
2015-06-02 13:44:54 -0500

    cuatro
    index,config,acciones

--- js/acciones.js
+++ js/acciones.js
@@ -1,6 +1,20 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
+	audio=window.plugins.LowLatencyAudio;
+	audio.preloadFX('B1','audio/C.mp3',function(){},
+	function(msg){alert("Error " + msg);});
+	
+	audio.preloadFX('B2','audio/D.mp3',function(){},
+	function(msg){alert("Error " + msg);});
+	
+	audio.preloadFX('B3','audio/E.mp3',function(){},
+	function(msg){alert("Error " + msg);});
+	
+	audio.preloadFX('B4','audio/F.mp3',function(){},
+	function(msg){alert("Error " + msg);});
+	
+	
 	$('#btnjugar').on('tap',function(){
 		var pantalla=$.mobile.getScreenHeight();
 		var encabezado=$('.ui-header').outerHeight();
@@ -27,6 +41,7 @@ document.addEventListener("deviceready",function(){
 	
 	function quien (q) 
 	{
+		audio.play(q);
 		return q.substring(1);
 	}
 }); 
